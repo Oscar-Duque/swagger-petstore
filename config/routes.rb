924e099ee23a0d7d812c7f8f16eebcea8aadd20a
Oscar Duque <dukescar@gmail.com>
2021-03-12 16:00:19 -0600

    first endpoint working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :v1 { format: :json } do
-    resources :pets, only: [ :index ]
+  namespace :v1, defaults: { format: :json } do
+    resources :pets
+    # , only: [ :index ]
   end
 end
