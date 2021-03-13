b77e82d6fde23db995a44540aecf85060bd0b812
Oscar Duque <dukescar@gmail.com>
2021-03-12 18:48:46 -0600

    created the second endpoint

--- app/controllers/v1/pets_controller.rb
+++ app/controllers/v1/pets_controller.rb
@@ -1,5 +1,20 @@
 class V1::PetsController < V1::BaseController
   def index
     @pets = Pet.all
+    # params[:limit]
+  end
+
+  def create
+    # TODO
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :tag)
   end
 end
